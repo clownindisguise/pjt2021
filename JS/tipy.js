0f30b94d41c83de3adf6a3038504abfe4714c9a0
clownindisguise <87302221+clownindisguise@users.noreply.github.com>
2021-07-14 20:53:52 +0200

    tipy4

--- JS/tipy.js
+++ JS/tipy.js
@@ -34,64 +34,64 @@ const asijskeDrama = [
     {
       nazev: "S láskou tě nenávidím",
       popis: "Sera neměla šťastné dětství. Její otec byl hazardní hráč, který prohrál veškerý jejich majetek. Matka trpí depresemi, je závislá na lécích a není schopná manžela opustit. Sera tak musela už od útlého věku živit nejen sebe, ale i svou matku a sestru, zatímco jí její otec kradl vše, co mu přišlo pod ruku. Ale usmálo se na ni štěstí, když potkala JJ a Bruce, starší homosexuální pár, který ji zaměstnal ve své restauraci. Navíc jí dovolili, aby bezplatně bydlela v jejich bytě. Nyní pendluje mezi restaurací a stáží v reklamní agentuře. Doufá, že tam získá trvalou pozici a konečně se osamostatní. Její sen se ale začne hroutit, když jednu bláznivou noc skončí v náručí neznámého muže, ze kterého se vyklube její nový šéf a soused. Jeho neodbytné flirtování a návrhy ohrožují její budoucnost a Sera neví, co si počít…",
-      video: "https://www.youtube.com/embed/9TIA-YVv4oA",
+      video: "../obalyknih/tipynavanoce/slaskoutenenavidim.jpg",
     },
     {
       nazev: "Bridgertonovi: Vévoda a já",
       popis: "Simon Basset, vévoda z Hastingsu, vymyslel plán jak se zbavit všech dohazovačů a vdavekchtivých dívek ze společnosti. Předstírá náklonnost k půvabné Daphne Bridgertonové, mladší sestře svého přítele, s níž uzavře dohodu, že se jí bude naoko dvořit. Přestože nemá v úmyslu se oženit, na té dívce je něco, co mu zrychluje srdeční tep. Daphne brzy zapomene, že jejich námluvy jsou pouhá přetvářka. Snad za to může okouzlující pohled jeho pronikavých očí nebo to, co cítí v jeho objetí, ale postupem času se do temperamentního vévody doopravdy zamiluje. Musí však chtě nechtě zabránit tomu, aby jí pohledný vévoda, který se zapřisáhl, že zůstane navždy svobodný, nezlomil srdce.",
-      video: "https://youtube.com/embed/1caZJtPXSME",
+      video: "../obalyknih/tipynavanoce/vevodaaja.jpg",
     },
     {
       nazev: "Pýcha a předsudek",
       popis: "Autorka líčí klidný a ničím nerušený život, všední radosti, zábavy a starosti. Rodinný klid bývá narušen zejména tehdy, když jsou v rodině dcery a na obzoru se objeví možný ženich. Vdavky jsou pro ženu v této době jedinou možností zaopatření a proto je nutné důmyslné splétání sítí kolem kandidáta ženitby. Autorka dává svému románu ironický podtext. Všechny postavy románu - stoický pan Bennet, jeho energická manželka a pět dcer vstoupily již dávno do podvědomí čtenářské veřejnosti...",
-      video: "https://youtube.com/embed/AfXToHcebio",
+      video: "../obalyknih/tipynavanoce/pychaapredsudek.jpg",
     },
     {
       nazev: "Síla stínů",
       popis: "Princezna Alyssa ana’Raisa, dědička trůnu královen z rodu Šedých vlčic, se cítí mnohem lépe s mečem v ruce než při plnění povinností u dvora. Po setkání se smrtí vyráží do útoku s cílem ukončit válku, která sužuje království po celý Lyssin život. Pokud její hra nevyjde, může přijít o své království, ještě než usedne na trůn. Za hranicemi, v nepřátelském Ardenu, je mladý kapitán Halston Matelon, jehož hvězda je na vzestupu, pověřován stále nebezpečnějšími úkoly. Ocitá se v neřešitelné situaci na hranici mezi životem a smrtí, mezi děsivou pověstí čarodějnic a vlčích válečníků na severu a krutým králem, který vládne jeho domovině. Poté, co se na scéně objeví nový nepřítel – síla, která hrozí, že zničí poslední paprsky naděje v troskách Sedmiříší –, dojde k nerozlučnému propojení složitě propletených osudů Lyss a Hala.",
-      video: "https://youtube.com/embed/nfK6UgLra7g",
+      video: "../obalyknih/tipynavanoce/silastinu.jpg",
     },
     {
       nazev: "Krutý princ",
       popis: "Vítejte ve světě, kterému vládnou nelítostné víly! První díl napínavé fantasy série. Jude bylo sedm, když jí zabili rodiče a unesli spolu se sestrami na proradný dvůr víl. O deset let později ale netouží Jude po ničem jiném než patřit mezi víly. A to i přes to, že je smrtelná. Většina víl lidmi pohrdá a nejvíce podlý princ Cardan. Aby Jude získala vysněné místo u dvora, musí se princi postavit. Zaplete se však do intrik, a když hrozí, že násilí zničí slavný dvůr, rozhodne se pro pochybné spojenectví, které ji může stát život.",
-      video: "https://youtube.com/embed/ed1xwAtF728",
+      video: "../obalyknih/tipynavanoce/krutyprinc.jpg",
     },
     {
       nazev: "",
       popis: "",
-      video: "https://youtube.com/embed//pJ3wd6u4zlQ",
+      video: "../obalyknih/tipynavanoce/.jpg",
     },
   ];
   const filmy = [
     {
       nazev: "Dopisy ztraceným",
       popis: "Maminka Juliet Youngové byla reportážní fotografka a cestovala po celém světě. Juliet si s ní vždycky psala dopisy. Dokonce i po její smrti jí na hrobě nechává psaníčka. Je to jediný způsob, jak se s tím Juliet dovede vyrovnat. Declan Murphy je ten typ kluka, na kterého nechcete narazit. Během veřejně prospěšných prací, které mu přikázal soud, se pokouší uprchnout démonům své minulosti. Když si Declan přečte dopis, který najde vedle hrobu, neodolá a odepíše. Netrvá to dlouho a už se svěřuje úplně cizímu člověku a je jasné, že mají opravdu hodně společného. Ale Declan ani Juliet netuší, s kým si ve skutečnosti píší. Když se do jejich dopisů zamotá dění ve škole a přeskočí jiskra, tak však Juliet s Declanem zjistí, že pravda by je mohla navždy rozdělit.",
-      video: "https://youtube.com/embed/fuPX8mjeb-E",
+      video: "../obalyknih/tipydodeste/dopisyztracenym.jpg",
     },
     {
       nazev: "Prokletý rok",
       popis: "Dívkám ve vesnici se tvrdí, že mají moc vyrvat spořádané muže z náruče jejich manželek. Proto jsou v šestnáctém roce života vyhnány do divočiny, kde se musí od této magie očistit – ne všechny se však vrátí. Tierney Jamesová sní o společnosti, kde mají ženy větší hodnotu než kus masa. Ale jak se blíží její vlastní prokletý rok, zjišťuje, že hrozba nečíhá jen v lesích. Nebezpečí se skrývá v nitru samotných dívek.",
-      video: "https://youtube.com/embed/oj2GSEkS9QI",
+      video: "../obalyknih/tipydodeste/prokletyrok.jpg",
     },
     {
       nazev: "Železná zkouška",
       popis: "Callum Hunt ví už odmalička, že pokud by se někdy zapletl s magií, znamenalo by to pro něj jediné – smrt. Jeho otec mu to ostatně nikdy nedovolí zapomenout. Když je Cal pozván k vstupním zkouškám do tajemné čarodějné školy jménem Magisterium, slíbí otci i sám sobě, že v testu dokonale propadne. Jenže Cal má naneštěstí magii v krvi, takže i přes veškerou snahu je spolu s dalšími dvěma žáky vybrán, aby studoval u největšího mága ze všech, mistra Rufuse. Cal zjišťuje, že magie je mnohem zábavnější, než si kdy uměl představit, a také skoro stejně tak nebezpečná. Ve škole si vedle nových přátel rychle udělá i nové nepřátele…",
-      video: "https://youtube.com/embed/E82xfU1Ptaw",
+      video: "../obalyknih/tipydodeste/zeleznazkouska.jpg",
     },
     {
       nazev: "Skleněný trůn",
       popis: "V temných a špinavých solných dolech Endovieru si doživotní trest odpykává osmnáctiletá dívka. Celaena je od dětství trénovaná k jednomu jedinému úkolu – stát se tím nejlepším zabijákem na světě, rychlým, tichým, neodhalitelným a všehoschopným. Ale udělala chybu. Nechala se chytit. Korunní princ jí nabídne poslední šanci: Svobodu výměnou za velkou oběť. Musí ho reprezentovat v turnaji, kde se proti sobě postaví 24 šampionů, těch nejlepších vrahů a zlodějů z celé země. Bude to boj na život a na smrt. Pokud zvítězí, bude volná. Ovšem její soupeři umírají nejen při soubojích, ale i za tajemných okolností – Celaena teď o svůj život bojuje nejen s nimi, ale i s něčím tajemným a děsivým, co se skrývá v pozadí.",
-      video: "https://youtube.com/embed/vPF3Y4dj_Fk",
+      video: "../obalyknih/sarahjmass/sklenenytrun.jpg",
     },
     {
       nazev: "Stmívání",
       popis: "Isabella Swan se stěhuje do Forks, městečka ve státě Washington, kde neustále prší. Zdá se jí, že na světě neexistuje nudnější a beznadějnější místo – až dokud nepotká tajuplného a přitažlivého Edwarda Cullena, který doslova obrátí její život vzhůru nohama. Do Isabellina příjezdu Edward úspěšně tajil před obyvateli městečka svou upírskou osobnost. Ale nikdo není v bezpečí, zvláště Isabella, člověk, který je Edwardovi nejdražší. Oba mladí lidé jsou k sobě vázáni silným citovým poutem. Zvítězí milostný cit, nebo hrůzné temné pudy?",
-      video: "https://youtube.com/embed/wsjUH8aAW9o",
+      video: "../obalyknih/tipydodeste/stmivani.jpg",
     },
     {
       nazev: "Píseň zimy",
       popis: "Liesl celé své dětství poslouchala krásné a tajemné příběhy o Králi skřetů – o nezbedném panovníkovi, který vládne podzemí, o Králi duchů, který ji inspiroval ke skládání hudby. Když ale Liesl vyrostla, musela své sny o králi a skládání hudby nechat stranou a věnovat se praktičtějším věcem. Avšak když zmizí její sestra Käthe, jen Liesl ví, že ji odnesli skřeti, a vydává se do podzemní říše, aby ji zachránila a přivedla zpět. Král podzemí jí sestru vydá pouze pod jistou podmínkou – za jeden život vyžaduje jiný. Aby zimu mohlo vystřídat jaro, musí si do podzemí vždy odvést pannu, a pokud se má Käthe vrátit, musí se pro ni Liesl obětovat. Bez smrti se nemůže svět znovu zrodit, a tak Liesl nabídne králi svou ruku. V podzemní říši Liesl zjistí, že ji král nadále inspiruje a že ho obdivuje. Avšak jak se její hudební talent rozvíjí, Liesl pomalu ztrácí sílu a jako králova nevěsta umírá. Čím blíž si ale s králem jsou, tím horší se zdá jejich rozhodnutí: Obětují svou lásku, anebo nastane konec světa?",
-      video: "https://youtube.com/embed/fgFPIh5mvNc",
+      video: "../obalyknih/tipydodeste/pisenzimy.png",
     },
   ];
   
