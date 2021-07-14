1341b708ffd9e0652f036f7c03d2f501eb0522bd
Nia2003 <ZabZaBloom@seznam.cz>
2021-07-14 14:16:53 +0200

    plk

---
+++ JS/tipy.js
@@ -0,0 +1,183 @@
+const asijskeDrama = [
+    {
+      nazev: "Doctors",
+      popis: "Doctors je drama, které jsem viděla jako druhé hned po Perfect And Casual. Je to také jediné drama, které jsem rozkoukala na YouTube a dokoukala na Netflixu.",
+      video: "https://www.youtube.com/embed/e5BHCHhr2Yw",
+    },
+    {
+      nazev: "You are my hero",
+      popis: "You Are My Hero je mé nejoblíbenější čínské drama, které jsem dokoukala. Díky tomuhle dramatu jsem našla stránku, na které se můžu koukat na skoro všechny známější seriály a filmy co se v Asii vysílají.",
+      video: "https://youtube.com/embed/3CZTTgQ5_6g",
+    },
+    {
+      nazev: "Love O2O",
+      popis: "Love 020 je mé první Čínské drama, které jsem viděla na Netflixu. NA tomto dramatu se mi líbí, že je ze školního prostředí a hlavní hrdinové hrají RPG hru, přes kterou se seznámí a poté se do sebe zamilují.",
+      video: "https://youtube.com/embed/tiiN4rDwa8g",
+    },
+    {
+      nazev: "My Holo Love",
+      popis: "My Holo Love je mé úplně první shlédnuté drama Na Netflixu. My Holo Love je romantické a psychologické drama, které v člověku zanechá hluboký dojem a člověk o tom pak dlouho přemýšlí. Jedním z hlavních témat tohoto drama je umělá inteligence.",
+      video: "https://youtube.com/embed/iBnc0x3t-hs",
+    },
+    {
+      nazev: "Perfect And Casual",
+      popis: "Perfect and Casual je mé první drama, které jsem shlédla celé. Celé tohle drama jsem shlédla na YouTube. Díky tomu, že jsem na tohle náhodou narazila, začala jsem sledovat asijská dramata.",
+      video: "https://youtube.com/embed/aM_3QD63Mc4",
+    },
+    {
+      nazev: "Run on",
+      popis: "Run On je jedno z mych prvních dramat, které jsem viděla. Run on je velice pohodové a oddechové drama.",
+      video: "https://youtube.com/embed/KVsok-tMcYw",
+    },
+  ];
+  const anime = [
+    {
+      nazev: "The Rising Of The Shield Hero / Tate no Yusha no Narigari",
+      popis: "The Rising Of The Shield Hero je mé nejoblíbenější akční fantasy anime. Toto anime je také současně mé první anime tohoto žánru. The Rising Of The Shield Hero připomíná trochu RPG hru. Hrdinové tohoto anime musí ",
+      video: "https://www.youtube.com/embed/9TIA-YVv4oA",
+    },
+    {
+      nazev: "Vampire Knight (+ Guilty)",
+      popis: "Vampire Knight je upíří anime a současně je to taktéž první anime co jsem viděla celé. Před tímto anime jsem viděla kus anime Diabolic Lovers, které mě od anime na nějakou dobu odradilo. Ale naštěstí mi kamarádka ukázala Vampire Knight. Díky tomuto anime jsem se stala milovník anime a mangi.",
+      video: "https://youtube.com/embed/1caZJtPXSME",
+    },
+    {
+      nazev: "Seraph of the End / Owari no Seraph",
+      popis: "Owari no Seraph je mé nejoblíbenější upírké postapokalyptické anime (možná to je tím, že to je jediné upírské postapokalyptické anime, které znám). Jakožto milovnice upíru a démonů jsem si toto anime velice užívala.",
+      video: "https://youtube.com/embed/AfXToHcebio",
+    },
+    {
+      nazev: "A Silent Voice / Koe no Katachi",
+      popis: "Koe no Katachi je můj první anime film. Děj je velice dojemný. Hlavními tématy tohoto anime je láska, šikana a přátelství. Díky hlavní hrdince tohoto anime jsem projevila menší zájem o znakovou řeč. Hlavní hrdinka je totiž hluchá a to jí velice znepříjemňije život a je pro ni těžké zapadnout do kolektivu.",
+      video: "https://youtube.com/embed/nfK6UgLra7g",
+    },
+    {
+      nazev: "Wolf Children / Ōkami Kodomo no Ame to Yuki",
+      popis: "Ōkami Kodomo no Ame to Yuki je podle mě nejlepší a nejdojemější anime film. Já jsem u tohoto anime proplakala začátek, střed i konec. Tot anime ve mě zanechalo opravdu hluboký dojem.",
+      video: "https://youtube.com/embed/ed1xwAtF728",
+    },
+    {
+      nazev: "Beastars",
+      popis: "Beastars je velice originální anime. Celé anime je podle mě vlastně jedna velká alegorie. Člověk o tom musí přemýšlet a pokaždé, co se na to anime člověk podívá, naléza v tom nové skrité významy a nebo v tom vidí úplně něco jiného než to, co viděl předtím.",
+      video: "https://youtube.com/embed//pJ3wd6u4zlQ",
+    },
+  ];
+  const filmy = [
+    {
+      nazev: "Bitva o Sevastopol",
+      popis: "Bitva o Sevastopol je Ruský film o nejlepší ruské odstřelovačce Ljudmile Pavličenkové, také známé jako Paní smrti. Filmje natočen podle knižní předlohy a je velice uvěřitelný, akční a dojemný.",
+      video: "https://youtube.com/embed/fuPX8mjeb-E",
+    },
+    {
+      nazev: "Twilight Saga",
+      popis: "Twilight Saga je série pěti filmů natočených podle čtyř knih: Stmívání, Nový měsíc, Zatmění a Rozbřezk. Filmy jsou velice podobný knížkám. Jsou tam sice určité rozdíly, ale i tak je to skvělá upíří sága.",
+      video: "https://youtube.com/embed/oj2GSEkS9QI",
+    },
+    {
+      nazev: "Better Days",
+      popis: "Better Days je Čínský film natočen podle skutečných událostí. Ve filmu je skvěle ukázáno prostředí škol v číně. Film je plný násilý a velice uvěřitelný. I když to není hezký film z pohledu žánru, je skvěle natočený.",
+      video: "https://youtube.com/embed/E82xfU1Ptaw",
+    },
+    {
+      nazev: "Underworld",
+      popis: "Underworld je filmová upíří série ubsahující pět filmů: Underworld; Underworld: Evolution; Underworld: Vzpoura Lykanů; Underworld: Probuzení a Underworld: Krvavé války. Jakožto Milovnice upírů romantiky a tajemna, jsem si tuto filmovou sérii velice užila.",
+      video: "https://youtube.com/embed/vPF3Y4dj_Fk",
+    },
+    {
+      nazev: "Interview s upírem",
+      popis: "Interview s upírem je film natočený podle knižní předlohy Princ Lestat od Anne Rice. Kniha i film jsou naprosto dokonalé, a film je téměř stejný jako knižní předloha.",
+      video: "https://youtube.com/embed/wsjUH8aAW9o",
+    },
+    {
+      nazev: "Dracula",
+      popis: "Dracula je film natočený podle knižní předlohy Dracula od Brama Stokera. I přes rozdíly mezi filmem a knihou. je film skvěle spracovaný. Tanto film jsem viděla už nesčetněkrát a vím, že ho ještě spostukrát shlédnu.",
+      video: "https://youtube.com/embed/fgFPIh5mvNc",
+    },
+  ];
+  
+  //asijske drama - slideshow
+  const tlPredchoziD = document.querySelector('#tlPredchoziD');
+  const tlNasledujiciD = document.querySelector('#tlNasledujiciD');
+  const tlPredchoziA = document.querySelector('#tlPredchoziA');
+  const tlNasledujiciA = document.querySelector('#tlNasledujiciA');
+  const tlPredchoziF = document.querySelector('#tlPredchoziF');
+  const tlNasledujiciF = document.querySelector('#tlNasledujiciF');
+  
+  
+  const sectionDrama = document.querySelector('#section-drama');
+  const sectionAnime = document.querySelector('#section-anime');
+  const sectionFilmy = document.querySelector('#section-filmy');
+  
+  let indexDrama = 0;
+  let indexAnime = 0;
+  let indexFilmy = 0;
+  
+  vypis(indexDrama, asijskeDrama, sectionDrama);
+  vypis(indexAnime, anime, sectionAnime);
+  vypis(indexFilmy, filmy, sectionFilmy);
+  
+  tlPredchoziD.addEventListener('click', function(){
+    if(indexDrama === 0){
+      indexDrama = asijskeDrama.length - 1;
+    }else{
+      indexDrama--;
+    }
+    vypis(indexDrama, asijskeDrama, sectionDrama);
+  });
+  
+  tlNasledujiciD.addEventListener('click', function(){
+    if(indexDrama === asijskeDrama.length - 1){
+      indexDrama = 0;
+    }else{
+      indexDrama++;
+    }
+    vypis(indexDrama, asijskeDrama, sectionDrama);
+  });
+  
+  tlPredchoziA.addEventListener('click', function(){
+    if(indexAnime === 0){
+      indexAnime = anime.length - 1;
+    }else{
+      indexAnime--;
+    }
+    vypis(indexAnime, anime, sectionAnime);
+  });
+  
+  tlNasledujiciA.addEventListener('click', function(){
+    if(indexAnime === anime.length - 1){
+      indexAnime = 0;
+    }else{
+      indexAnime++;
+    }
+    vypis(indexAnime, anime, sectionAnime);
+  });
+  
+  tlPredchoziF.addEventListener('click', function(){
+    if(indexFilmy === 0){
+      indexFilmy = filmy.length - 1;
+    }else{
+      indexFilmy--;
+    }
+    vypis(indexFilmy, filmy, sectionFilmy);
+  });
+  
+  tlNasledujiciF.addEventListener('click', function(){
+    if(indexFilmy === filmy.length - 1){
+      indexFilmy = 0;
+    }else{
+      indexFilmy++;
+    }
+    vypis(indexFilmy, filmy, sectionFilmy);
+  });
+  
+  
+  function vypis(index, data, sekce){
+    const elNazev = sekce.querySelector('.nazev');
+    const elPopisek = sekce.querySelector('.popisek');
+    const elVideo = sekce.querySelector('.video');
+  
+    elNazev.textContent = data[index].nazev;
+    elPopisek.textContent = data[index].popis;
+    elVideo.setAttribute('src', data[index].video);
+  }
+  
